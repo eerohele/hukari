9360aa7600c9386be5ae3a31573143e23a3d0326
Eero Helenius <eero.helenius@solita.fi>
2022-02-27 22:08:24 +0200

    Improve search-maven-central fn

--- src/hukari/repl.clj
+++ src/hukari/repl.clj
@@ -19,8 +19,7 @@
 
    (let [query (read-line)]
      (when-not (= ":q" query)
-       (let [_ (println query)
-             _ (println)
+       (let [_ (println)
              uri (URI/create (format "https://search.maven.org/solrsearch/select?q=%s&rows=%d&wt=xml" (URLEncoder/encode query) max-results))
              request (.. request-builder (uri uri) (build))
              response (.send http-client request body-handler)
@@ -28,25 +27,24 @@
              coordinates (mapv #(let [id (-> % :content (nth 3) (get :content) first (string/replace #":" "/") symbol)
                                       version (-> % :content (nth 4) (get :content) first)]
                                   {id {:mvn/version version}})
-                           (-> result-zipper zip/down zip/right zip/children))
-             sb (StringBuilder.)
-             _ (doall
-                 (map-indexed (fn [index coord]
-                                (.append sb (format "%02d" (inc index)))
-                                (.append sb ". ")
-                                (.append sb (pr-str coord))
-                                (.append sb \newline))
-                   coordinates))
-             _ (println (.toString sb))
-             _ (print "Enter the number of the artifact whose coordinates to return or :q to abort: ")
-             _ (flush)
-             choice (read)]
-         (if (= :q choice)
-           (println "aborting...")
-           (do
-             (println (format "%02d" choice))
-             (println)
-             (get coordinates (dec choice)))))))))
+                           (-> result-zipper zip/down zip/right zip/children))]
+         (if (empty? coordinates)
+           (printf "No results for %s.\n" query)
+           (let [sb (StringBuilder.)
+                 _ (doall
+                     (map-indexed (fn [index coord]
+                                    (.append sb (format "%02d" (inc index)))
+                                    (.append sb ". ")
+                                    (.append sb (pr-str coord))
+                                    (.append sb \newline))
+                       coordinates))
+                 _ (println (.toString sb))
+                 _ (print "Enter the number of the artifact whose coordinates to return or :q to abort: ")
+                 _ (flush)
+                 choice (read)]
+             (if (= :q choice)
+               (println "aborting...")
+               (get coordinates (dec choice))))))))))
 
 (comment
   (search-maven-central {:max-results 5})
@@ -66,7 +64,8 @@
             response (.send http-client request body-handler)
             result-zipper (-> response .body xml/parse zip/xml-zip)
             {:keys [group_name jar_name version]} (-> result-zipper zip/down zip/node :attrs)]
-        {(symbol group_name jar_name) {:mvn/version version}}))))
+        (println)
+        (doto {(symbol group_name jar_name) {:mvn/version version}} prn)))))
 
 (comment
   (search-clojars)
