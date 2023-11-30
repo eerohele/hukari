26cd760ee55f7c2ee96e6f1b54716f899e813ee3
Eero Helenius <eero.helenius@yle.fi>
2023-11-30 23:03:13 +0200

    Add time+ macro

--- src/hukari/repl.clj
+++ src/hukari/repl.clj
@@ -612,3 +612,41 @@
     (.put 3)
     (.take))
   ,,,)
+
+;; Adapted from https://clojure-goes-fast.com/kb/benchmarking/time-plus/
+(let [time*
+      (fn [^long duration-in-ms f]
+        (let [^com.sun.management.ThreadMXBean bean (ManagementFactory/getThreadMXBean)
+              bytes-before (.getCurrentThreadAllocatedBytes bean)
+              duration (* duration-in-ms 1000000)
+              start (System/nanoTime)
+              ret (f)
+              delta (- (System/nanoTime) start)
+              deadline (+ start duration)
+              tight-iters (max (quot (quot duration delta) 10) 1)]
+          (loop [iterations 1]
+            (let [now (System/nanoTime)]
+              (if (< now deadline)
+                (do (dotimes [_ tight-iters] (f))
+                  (recur (+ iterations tight-iters)))
+                (let [i' (double iterations)
+                      bytes-after (.getCurrentThreadAllocatedBytes bean)
+                      t (/ (- now start) i')
+                      time (cond (< t 1e3) (format "%.0f ns" t)
+                             (< t 1e6) (format "%.2f µs" (/ t 1e3))
+                             (< t 1e9) (format "%.2f ms" (/ t 1e6))
+                             :else (format "%.2f s" (/ t 1e9)))
+                      allocations (/ (- bytes-after bytes-before) i')]
+                  (println {:time time
+                            :allocations allocations
+                            :iterations iterations})
+                  ret))))))]
+  (defmacro time+
+    "Like `time`, but runs the supplied body for 2000 ms and prints the average
+  time for a single iteration. Custom total time in milliseconds can be provided
+  as the first argument. Returns the returned value of the FIRST iteration."
+    [?duration-in-ms & body]
+    (let [[duration body] (if (integer? ?duration-in-ms)
+                            [?duration-in-ms body]
+                            [2000 (cons ?duration-in-ms body)])]
+      `(~time* ~duration (fn [] ~@body)))))
