194293fc67e07b16c436929955e468770424cfd2
Eero Helenius <31859+eerohele@users.noreply.github.com>
2025-05-01 20:58:33 +0300

    Add first cut of inspect helper
    Copied from tutkain.repl.loop.

--- src/hukari/repl.clj
+++ src/hukari/repl.clj
@@ -708,3 +708,74 @@
         (.append sb (hex (bit-shift-right v 4)))
         (.append sb (hex (bit-and v 0x0F)))))
     (.toString sb)))
+
+(defn inspect
+  "Given a (presumably nested) coll, start a loop for navigating the coll.
+
+  Reads from *in*, then prints coll at accumulated keypath.
+
+  Input ? for help.
+
+  For all other inputs, print value at (get-in coll (conj keypath key))."
+  [coll & {:keys [print print-length]
+           :or {print prn print-length 16}}]
+  (binding [*print-length* print-length]
+    (print coll)
+    (loop [keypath []]
+      (printf "%s=> " keypath)
+      (flush)
+      (let [input (read)]
+        (println input)
+        (cond
+          (= input 'q)
+          :bye
+
+          (= input '?)
+          (do
+            (println "Commands:
+
+    ? — show this help
+    q — quit
+    . — show current value (without truncation)
+    - — go up one level
+          ")
+            (recur keypath))
+
+          :else (let [v (get-in coll keypath)]
+                  (cond
+                    (and (= input '-) (empty? keypath))
+                    (do
+                      (print :top)
+                      (print v)
+                      (recur keypath))
+
+                    (= input '-)
+                    (let [keypath (pop keypath)]
+                      (print (get-in coll keypath))
+                      (recur keypath))
+
+                    (= input '.)
+                    (do
+                      (binding [*print-length* nil]
+                        (print v))
+                      (recur keypath))
+
+                    (or (map? v) (indexed? v))
+                    (let [new-keypath (conj keypath input)]
+                      (if (contains? (get-in coll keypath) input)
+                        (do (print (get-in coll new-keypath))
+                          (recur new-keypath))
+                        (do (print :nope)
+                          (print (get-in coll keypath))
+                          (recur keypath))))
+
+                    (and (seqable? v) (nat-int? input) (< input (count v)))
+                    (do
+                      (print (nth v input))
+                      (recur keypath))
+
+                    :else
+                    (do
+                      (print :nope)
+                      (print v)
+                      (recur keypath)))))))))
